27dec056295ece9f6442d1a95a94eeef1727bc1e
Joshua <mugishajosuetrezorhp@gmail.com>
2024-12-12 11:31:13 +0200

    configured routes

--- src/components/headerTwo.jsx
+++ src/components/headerTwo.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
-// import "./../assets/livematchmodal.css";
 
 const HeaderTwo = () => {
   const navigation = [
@@ -11,34 +9,40 @@ const HeaderTwo = () => {
     { name: 'MATCHES', path: '/match' },
     { name: 'INFRASTRUCTURE', path: '/infrastructures' },
   ];
-    const [activeTab, setActiveTab] = useState('HOME');
+  const [iSactiveTab, setIsActiveTab] = useState('HOME');
+
+  const handleTabClick = (name) => {
+    setIsActiveTab(name);
+  };
 
   return (
     <div>
-        <header className="bg-white shadow fixed top-0 w-full z-50">
+      <header className="bg-white shadow fixed top-0 w-full z-50">
         <div className="container mx-auto px-6">
           <div className="flex items-center justify-between h-20">
-            <Link
-            to="/landing"
-            >
-            <img src="/logo/logo.svg" alt="MINISPORTS" className="h-12 w-auto" />
+            <Link to="/landing">
+              <img src="/logo/logo.svg" alt="MINISPORTS" className="h-12 w-auto" />
             </Link>
             <nav className="hidden md:flex space-x-10">
-      {navigation.map((item) => (
-        <Link
-          to={item.path}
-          key={item.name}
-          className={`px-6 py-2.5 rounded-lg text-base ${
-            activeTab === item.name
-              ? 'bg-blue-600 text-white'
-              : 'text-gray-600 hover:bg-gray-100'
-          }`}
-          onClick={() => setActiveTab(item.name)}
-        >
-          {item.name}
-        </Link>
-      ))}
-    </nav>
+              {navigation.map((item) => {
+                const isActiveTab = location.pathname === item.path;
+                return(
+                <Link
+                  to={item.path}
+                  key={item.name}
+                  className={`px-6 py-2.5 rounded-lg text-base ${
+                    isActiveTab
+                      ? 'bg-blue-600 text-white'
+                      : 'text-gray-600 hover:bg-gray-100'
+                  }`}
+                  onClick={() => {
+                    handleTabClick(item.name);
+                  }}
+                >
+                  {item.name}
+                </Link>
+                )})}
+            </nav>
 
             <Link
               to="/login"
