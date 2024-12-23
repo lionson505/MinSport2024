c888d0001818d8b9bf6995b640487c521bd20d5a
lionson505 <143701468+lionson505@users.noreply.github.com>
2024-12-23 15:05:34 +0200

    some chart comment

--- src/components/charts/LineChart.jsx
+++ src/components/charts/LineChart.jsx
@@ -2,6 +2,7 @@ import { Line } from 'react-chartjs-2';
 import { lineChartOptions } from '../../config/chartConfig';
 
 export function LineChart({ data, options = {} }) {
+  // some comment 
   return (
     <Line 
       data={data} 
